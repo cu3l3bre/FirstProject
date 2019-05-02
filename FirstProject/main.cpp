981133916a90e1415e3bb2d04977612bb84381c9
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-02 13:20:42 +0200

    Projektdateien hinzufügen.

---
+++ FirstProject/main.cpp
@@ -0,0 +1,24 @@
+
+// Einbinden von "iostream"
+#include <iostream>
+
+// den Standardnamenraum verfügbar machen
+using namespace std;
+
+
+// Einstiegspunkt der Anwendung
+int main()
+{
+
+	// den Text "Hello World" auf dem Bildschirm anzeigen
+	cout << "Hello World!" << endl;
+
+
+	// Hilfsangabe, um die Konsole offen zu halten (so nur unter Windows)
+	system("pause");
+
+
+
+	// Rückgabewert an das Betriebssystem, dass das Programm ohne Fehler beendet wurde
+	return 0;
+}
